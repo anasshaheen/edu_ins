39e79e3b982391dbf28d137c314ac311cdc07e68
Anas Shaheen <anassh3221@gmail.com>
2019-07-31 20:53:45 +0300

    fix dbConfig

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ interface DBOptions {
 
 const dbOptions: DBOptions = {
   DATABASE_URI: HostingEnvironment.isTesting(<string>process.env.NODE_ENV)
-    ? 'mongodb://localhost:27017/edu_ins_test' //<string>process.env.TEST_DATABASE_URI
+    ? <string>process.env.TEST_DATABASE_URI
     : <string>process.env.DATABASE_URI,
 };
 
